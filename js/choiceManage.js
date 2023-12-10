5703ab69a047f04a21dc6959d99724c66a906f38
TOTON Lionel <totonlionel@gmail.com>
2023-12-10 13:55:45 +0100

    Le calculateur refait

---
+++ js/choiceManage.js
@@ -0,0 +1,14 @@
+flatpickr("#datePicker", {
+  // enableTime: true,
+    altInput: true,
+  altFormat: "F j, Y",
+     dateFormat: "Y-m-d",
+     minDate: "today"
+});
+
+flatpickr("#dateSelect", {
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+  minDate: "today"
+});
