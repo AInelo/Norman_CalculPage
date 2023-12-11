625254cfbe9de60f250a33971ff86fbc55cba6f7
TOTON Lionel <totonlionel@gmail.com>
2023-12-11 08:37:20 +0100

    Le calculateur refait

--- js/choiceManage.js
+++ js/choiceManage.js
@@ -1,14 +1,16 @@
-flatpickr("#datePicker", {
-  // enableTime: true,
-    altInput: true,
-  altFormat: "F j, Y",
-     dateFormat: "Y-m-d",
-     minDate: "today"
-});
-
-flatpickr("#dateSelect", {
-  altInput: true,
-  altFormat: "j F, Y",
-  dateFormat: "Y-m-d",
-  minDate: "today"
-});
+// flatpickr("#datePicker", {
+//   // enableTime: true,
+//     altInput: true,
+//   altFormat: "F j, Y",
+//      dateFormat: "Y-m-d",
+//      minDate: "today"
+// });
+//
+// flatpickr("#dateSelect", {
+//   altInput: true,
+//   altFormat: "j F, Y",
+//   dateFormat: "Y-m-d",
+//   minDate: "today"
+// });
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});
